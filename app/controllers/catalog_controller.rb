d3dee537fb43d4f96d54cc98b7ad5123454b231c
ianmilligan1 <i2milligan@uwaterloo.ca>
2017-05-15 17:08:22 -0400

    removed revisits for test index

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -17,6 +17,7 @@ class CatalogController < ApplicationController
 
     ## Default parameters to send to solr for all search-like requests. See also SearchBuilder#processed_parameters
     config.default_solr_params = {
+      :fq => ['record_type:response'],
       rows: 10
     }
 
